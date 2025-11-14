f7bf45d3f0bb784b73e66dad74b3a1bc2f4c4e1c
NuwanKeshara <nuwankyazh@gmail.com>
2025-11-14 10:42:34 +0200

    add search in a 2D matrix problem

--- NeetCode/binary_search.py
+++ NeetCode/binary_search.py
@@ -13,7 +13,7 @@ class Solution:
                 last = mid - 1
         return -1
     
-    # memory overflow fixed
+    # memory overflow fixed BS
     def search(self, nums:list[int], target:int) -> int:
         first, last = 0, len(nums)-1
 
@@ -31,10 +31,35 @@ class Solution:
     
 
 
+    def searchMatrix(self, matrix:list[list[int]], target:int) -> bool:
+        # matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
+        rows, columns = len(matrix), len(matrix[0])
+        left, right = 0, (rows * columns) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // columns
+            column = (mid % columns)
+
+            if matrix[row][column] < target:
+                left = mid + 1
+            elif matrix[row][column] > target:
+                right = mid - 1
+            else:
+                return True
+                  
+        return False
+
+
+    
+
+
 
 if __name__ == "__main__":
     sol = Solution()
 
 
     # print(sol.evalRPN(["4","13","5","/","+"]))
-    print(sol.search([-1,0,2,4,6,8], 3))
+    # print(sol.search([-1,0,2,4,6,8], 3))
+
+    print(sol.searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 41))
