4d3d55c0785a7f9b611627f4e532a819e3b89509
NuwanKeshara <nuwankyazh@gmail.com>
2025-11-16 00:07:24 +0200

    add reverse linkedlist problem with 2 pointers

--- NeetCode/linked_list.py
+++ NeetCode/linked_list.py
@@ -1,1 +1,41 @@
-print("hello")
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        left, right = None, head
+
+        while right:
+            temp = right.next
+            right.next = left
+            left = right
+            right = temp
+        
+        return left
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    ln3 = ListNode(3,None)
+    ln2 = ListNode(2,ln3)
+    ln1 = ListNode(1,ln2)
+    ln0 = ListNode(0,ln1)
+
+    s = Solution()
+    x = s.reverseList(ln0)
+    while x:
+        print(x.val)
+        x = x.next
+
