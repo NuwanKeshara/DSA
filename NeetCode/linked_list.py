02867479a899db2c2a17232ef3c7f52764b5b4c7
NuwanKeshara <nuwankyazh@gmail.com>
2025-11-16 15:03:00 +0200

    add linkedlist python

--- NeetCode/linked_list.py
+++ NeetCode/linked_list.py
@@ -20,22 +20,90 @@ class Solution:
         return left
 
 
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+        else:
+            if list1.val <= list2.val:
+                head, tail = list1, list1
+                curr1 = list1.next
+                curr2 = list2
+            else:
+                head, tail = list2, list2
+                curr2 = list2.next
+                curr1 = list1
+            
+            while curr1 and curr2:
+
+                if curr1.val <= curr2.val:
+                    tail.next = curr1
+                    curr1 = curr1.next
+                else:
+                    tail.next = curr2
+                    curr2 = curr2.next
+                tail = tail.next
+            
+            if curr1:
+                tail.next = curr1
+            elif curr1:
+                tail.next = curr2
+            
+            return head
 
 
 
 
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add(self, val) -> Node:
+        curr = Node(val)
+
+        if not self.head:
+            self.head = curr
+            self.tail = curr
+        else: 
+            self.tail.next = curr
+            self.tail = curr
+        return self.head
+    
+    # def __repr__(self):
+    #     return f"{self.head}"
+    
+    def __str__(self):
+        vals = []
+        curr = self.head
+        while curr is not None:
+            vals.append(curr.val)
+            curr = curr.next
+        return str(vals)
 
 
 
 if __name__ == "__main__":
-    ln3 = ListNode(3,None)
-    ln2 = ListNode(2,ln3)
-    ln1 = ListNode(1,ln2)
-    ln0 = ListNode(0,ln1)
+    # ln3 = ListNode(3,None)
+    # ln2 = ListNode(2,ln3)
+    # ln1 = ListNode(1,ln2)
+    # ln0 = ListNode(0,ln1)
 
-    s = Solution()
-    x = s.reverseList(ln0)
-    while x:
-        print(x.val)
-        x = x.next
+    # s = Solution()
+    # x = s.reverseList(ln0)
+    # while x:
+    #     print(x.val)
+    #     x = x.next
 
+    ll = LinkedList()
+    ll.add(1)
+    # ll.add(2)
+    # ll.add(3)
+
+    print(ll)
