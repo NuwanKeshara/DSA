ebdc7261cb3bec7e5df7bb96de7e76912d5d5aee
NuwanKeshara <nuwankyazh@gmail.com>
2025-11-19 12:40:35 +0200

    add Binary Serach Tree

---
+++ NeetCode/tree.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, val: int =0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class BinaryTree:
+    def __init__(self):
+        self.head = None
+    
+    def add(self, val=0):
+        node = TreeNode(val=val)
+
+        if self.head:
+            curr = self.head
+            while True:
+                if curr.val > val:
+                    if curr.left:
+                        curr = curr.left
+                    else:
+                        curr.left = node
+                        break
+                else:
+                    if curr.right:
+                        curr = curr.right
+                    else:
+                        curr.right = node
+                        break
+        else:
+            self.head = node
