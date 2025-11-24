5525a5e78c17af095cf8d86e08189c8a437cba89
NuwanKeshara <nuwankyazh@gmail.com>
2025-11-25 00:58:23 +0200

    add preorder traversal for binary tree

--- NeetCode/tree.py
+++ NeetCode/tree.py
@@ -63,8 +63,33 @@ class BinaryTree:
         return res
     
 
-    def preOrder(self):
-        ...
+    def preOrder(self, root: Optional[TreeNode]) -> list[int]:
+        res = []
+
+        def preorder(root):
+            if not root:
+                return
+            res.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        
+        preorder(root)
+        return res
+    
+    def preOrder_iterative(self, root: Optional[TreeNode]) -> list[int]:
+        res = []
+        stack = []
+        curr = root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                res.append(curr.val)
+                curr = curr.left
+            curr = stack.pop()
+            curr = curr.right
+        return res
+
 
     def postOrder(self):
         ...
@@ -75,7 +100,7 @@ class BinaryTree:
 
     def __str__(self):
         print("InOrder Traversl:", str(self.inOrder(self.head)))
-        print("PreOrder Traversl:")
+        print("PreOrder Traversl:", str(self.preOrder(self.head)))
         print("PostOrder Traversl:")
         print("BFS Traversl:")
         return ""
