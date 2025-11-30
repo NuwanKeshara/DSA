b5e39a0c370f73bf468ddc7108196bf65e211522
NuwanKeshara <nuwankyazh@gmail.com>
2025-11-30 23:06:13 +0200

    add diameter of a binary tree problem

--- NeetCode/tree.py
+++ NeetCode/tree.py
@@ -239,7 +239,19 @@ class BinaryTree:
         
 
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        ...
+        self.res = 0
+
+        def dfs(curr):
+            if not curr:
+                return 0
+            left = dfs(curr.left)
+            right = dfs(curr.right)
+
+            self.res = max(self.res, left + right)
+            return 1 + max(left, right)
+        
+        dfs(root)
+        return self.res
 
 
 
