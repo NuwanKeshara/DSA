e40bca7fc06a0d3fde069211e47311dc1af6b28a
NuwanKeshara <nuwankyazh@gmail.com>
2025-11-30 22:33:19 +0200

    add recursive

---
+++ NeetCode/main.py
@@ -0,0 +1,30 @@
+def sumOfAall(idx):
+    if idx == 1:
+        return 1
+    else:
+        return idx + sumOfAall(idx - 1)
+    
+# print(sumOfAall(5))
+
+# 0, 1, 1, 2, 3, 5, 8, 13...
+def fibonacci(num):
+    if num <= 2:
+        return num - 1
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+    
+# print(fibonacci(4))
+total = 0
+
+def sum(num):
+    # print("hello")
+    if num == 0:
+        return 0
+    else:
+        # global total
+        # total += num
+        print("hello")
+        return sum(num-1) 
+    
+print(sum(5))
+print(total)
